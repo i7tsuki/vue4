0e5ab95bb00d8fa204355622942944e149d29170
i7tsuki <tanaka_yi7@yahoo.co.jp>
2020-08-13 19:04:28 +0900

    コンポーネント追加（プッシュ漏れ）

---
+++ src/components/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="login">
+    <h1>ログイン画面</h1>
+    <p><label>メールアドレス<input type="email" placeholder="E-mail"></label></p>
+    <p><label>パスワード<input type="text" placeholder="Password"></label></p>
+    <p><button @click="login">ログイン</button></p>
+    <a @click="GotoSignup"><small>新規登録はこちらから</small></a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  methods: {
+    login: function() {
+      alert('まだ実装していません。');
+    },
+    GotoSignup: function() {
+      this.$router.push('/signup');
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #4a42b9;
+}
+</style>
---
+++ src/components/Signup.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="Signup">
+    <h1>新規登録画面</h1>
+    <p><label>ユーザー名<input type="text" placeholder="userName" v-model="userName"></label></p>
+    <p><label>メールアドレス<input type="email" placeholder="E-mail" v-model="mail"></label></p>
+    <p><label>パスワード<input type="text" placeholder="Password" v-model="password"></label></p>
+    <p><button @click="signup">新規登録</button></p>
+    <a @click="GotoLogin"><small>ログインはこちらから</small></a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signup',
+  data: function() {
+    return {
+      userName: '',
+      mail: '',
+      password: ''  
+    }
+  },
+  methods: {
+    signup: function() {
+      this.$store.commit('createUserAccount', {
+        'userName': this.userName, 'mail': this.mail, 'password': this.password
+      });
+    },
+    GotoLogin: function () {
+      this.$router.push('/');
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #4a42b9;
+}
+</style>
