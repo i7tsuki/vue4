2cfcf98025ce42ea0eec2cce1885b01c64b17e57
i7tsuki <tanaka_yi7@yahoo.co.jp>
2020-08-14 21:47:21 +0900

    config作成（プッシュ漏れ）

---
+++ src/config/firebase-config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCUt5FGqhpPwZboGxhcm5jG-QzR-mAfm5c",
+  authDomain: "vue4-93291.firebaseapp.com",
+  databaseURL: "https://vue4-93291.firebaseio.com",
+  projectId: "vue4-93291",
+  storageBucket: "vue4-93291.appspot.com",
+  messagingSenderId: "92673480718",
+  appId: "1:92673480718:web:0afd52257dfd99b8a6045f"
+};
