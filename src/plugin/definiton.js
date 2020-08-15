d27867fd09c3cb54c2ea93e1023d42a1e7a5df52
i7tsuki <tanaka_yi7@yahoo.co.jp>
2020-08-16 08:34:59 +0900

    test()メソッドが真偽値を返すのでそのまま返すよう修正

--- src/plugin/definiton.js
+++ src/plugin/definiton.js
@@ -1,8 +1,3 @@
 export function isMailAdress(str) {
-  if (!(/^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/).test(str)) {
-    return false;
-  } else {
-    return true;
-  }
+  return (/^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/).test(str);
 }
-  
